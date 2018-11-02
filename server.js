eee480a98ba6edf7e539d6a7689e45f4f4bf4018
Wyatt <mrwnt10@gmail.com>
2018-11-02 14:50:42 -0400

    Handle URLs with listing in path

--- server.js
+++ server.js
@@ -8,5 +8,7 @@ app.use(morgan('dev'));
 
 app.use(express.static(path.join(__dirname, './public')));
 
+app.get('/*', ((res, req) => req.sendFile(path.join(__dirname, './public/index.html'))));
+
 var port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server now listening on port ${port}`));
